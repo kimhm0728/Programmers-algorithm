5aaced1325bd6fb1736957a41783ba2eddd64350
kimhm0728 <kimhm0728@naver.com>
2022-12-29 23:19:47 +0900

    add files

--- Programmers/src/Kakao2018/Level2_3_4.java
+++ Programmers/src/Kakao2018/Level2_3_4.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class Level2_3_4 {
 	// 2018 KAKAO BLIND RECRUITMENT 3차 Level 2 네 번째 문제 방금그곡
 
+    static String[][] alpha = {{"A#", "a"}, {"G#", "g"}, {"F#", "f"}, {"D#", "d"}, {"C#", "c"}};
     static String ans = "(None)";
     static int ansTime = 0;
 	
@@ -16,8 +17,8 @@ public class Level2_3_4 {
     static String solution(String m, String[] musicinfos) {
     	String target = replaceMusic(m);
     	
-    	for(String musicinfo : musicinfos) {
-            String[] music = getMusic(musicinfo);
+    	for(int i=0;i<musicinfos.length;i++) {
+            String[] music = getMusic(musicinfos[i]);
             isSameMusic(target, music[0], music[1]); // 네오가 기억한 멜로디, 제목, 재생된 음
         }
     	
@@ -26,47 +27,42 @@ public class Level2_3_4 {
     
     static String[] getMusic(String str) {
     	StringTokenizer st = new StringTokenizer(str, ",");
-        int start = strToInt(st.nextToken());
-        int end = strToInt(st.nextToken());
+        int start = strToInt(st.nextToken()); // 시작 시간
+        int end = strToInt(st.nextToken()); // 끝 시간
         int time = end - start;
         
-        String title = st.nextToken();
-        String music = replaceMusic(st.nextToken());
+        String title = st.nextToken(); 
+        String music = replaceMusic(st.nextToken()); // 곡의 음
 
         int len = music.length();
         String[] ret = new String[2];
         ret[0] = title;
         ret[1] = music.repeat(time / len) + music.substring(0, time % len);
+        // 재생 시간을 곡 길이로 나눈 몫 만큼 반복, 반복 후 남은 시간이 있다면 붙이기
         
         return ret;
     }
     
+    // HH:MM 형태의 시간을 분으로 변환
     static int strToInt(String str) {
         StringTokenizer st = new StringTokenizer(str, ":");
         return Integer.parseInt(st.nextToken()) * 60 + Integer.parseInt(st.nextToken());
     }
     
+    // # 들어가는 음을 다른 알파벳으로 치환
     static String replaceMusic(String m) {
     	String ret = m;
-    	char alpha = 'A';
-    	for(int i=0;i<7;i++) 
-    		ret = ret.replaceAll(Character.toString(alpha + i) + "#", Character.toString(alpha + 32 + i));
+    	for(String[] a : alpha)
+            ret = ret.replaceAll(a[0], a[1]);
     	
-    	return ret;
+        return ret;
     }
     
     static void isSameMusic(String target, String title, String music) {
-    	Loop : for(int i=0;i<music.length();i++) {
-    		if(music.charAt(i) == target.charAt(0)) {
-    			for(int j=1;j<target.length();j++)
-    				if(target.charAt(j) != music.charAt((i + j) % music.length()))
-    					continue Loop;
-    	    	if(ansTime < music.length()) {
-                    ans = title;
-                    ansTime = music.length();
-                }
-    	    	return;
-    		}
-    	}
+        int len = music.length();
+    	if(music.contains(target) && ansTime < len) {
+            ans = title;
+            ansTime = len;
+        }
     }
 }
